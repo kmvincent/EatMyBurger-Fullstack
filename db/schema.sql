111c36ff96b7ab4f1c1b1b8985be52a7f7490bab
Kathleen Vincent <kvincen4316@gmail.com>
2018-07-26 22:50:21 -0500

    burger app

--- db/schema.sql
+++ db/schema.sql
@@ -12,4 +12,10 @@ CREATE TABLE burgers
 	PRIMARY KEY (id)
 );
 
+
+INSERT INTO burgers (burger_name, devoured) values ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) values ('Juicy Lucy', true);
+INSERT INTO burgers (burger_name, devoured) values ('Double Decker', false);
+INSERT INTO burgers (burger_name, devoured) values ('Bacon Burger', false);
+
 SELECT * FROM burgers;
